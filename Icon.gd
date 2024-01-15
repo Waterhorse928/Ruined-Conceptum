fb35b4d322c9393b28979721ddd101299b995b21
SilverSmith459 <tyneville12@gmail.com>
2024-01-15 09:31:47 -0800

    hello world
    Can't go wrong with tradition

--- Icon.gd
+++ Icon.gd
@@ -1,9 +1,15 @@
 extends Sprite3D
 
+var coins = 5
+var player_name = "robot"
+var hearts = 3.5
+const SPEED = 2
+# This is incredible
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print("hello world")
+	# Gotta stick to tradition
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
