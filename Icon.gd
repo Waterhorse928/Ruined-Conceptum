29576dc8017f8d96dbcdc4623588bfa5133710a7
SilverSmith459 <tyneville12@gmail.com>
2024-01-15 12:47:55 -0800

    It's technically a game now

--- Icon.gd
+++ Icon.gd
@@ -9,9 +9,20 @@ const SPEED = 2
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("hello world")
+	print(add_numbers(5, 6))
 	# Gotta stick to tradition
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	check_inputs()
+
+func add_numbers(x, y):
+	var sum = x + y
+	return sum
+	
+func check_inputs():
+	if Input.is_action_pressed("ui_left"):
+		rotate_y(deg_to_rad(-SPEED))
+	elif Input.is_action_pressed("ui_right"):
+		rotate_y(deg_to_rad(SPEED))
