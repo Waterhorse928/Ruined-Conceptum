f3e0dee8318251be7b6f4ee73460aad5b2318985
SilverSmith459 <tyneville12@gmail.com>
2024-01-16 14:59:19 -0800

    Level Transitions Test
    It works. Also collision on the basic level design we did.

---
+++ coin 2.gd
@@ -0,0 +1,22 @@
+extends Area3D
+
+const ROT_SPEED = 2
+# Number of degrees the coin rotates per frame
+
+	# This is like autoloading the scene, only
+	# it happens after already loading the main scene.
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	rotate_y(deg_to_rad(ROT_SPEED))
+	
+
+
+func _on_body_entered(body):
+	get_tree().change_scene_to_file("res://level_1.tscn")
--- coin.gd
+++ coin.gd
@@ -3,6 +3,10 @@ extends Area3D
 const ROT_SPEED = 2
 # Number of degrees the coin rotates per frame
 
+	# This is like autoloading the scene, only
+	# it happens after already loading the main scene.
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,4 +19,4 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	queue_free()
+	get_tree().change_scene_to_file("res://level_2.tscn")
